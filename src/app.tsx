a42d2c5aff3663a07843cc714d0e781d702bd21a
mateus sant'ana <mateusfdev@outlook.com>
2024-02-09 15:13:42 -0300

    create input style 🏅🥇

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,18 @@ import logo from './assets/logo.svg'
 //components react são funções que retorna html em formato JSX
 export function App() {
   return (
-    <img src={logo} alt="" />
+    <div className='mx-auto max-w-6xl my-12'>
+      <img src={logo} alt="Logo site'bloco de notas.voip'" />
+
+      <form className='w-full'>
+        <input
+          type="text"
+          placeholder='busque suas notas...'
+          className='w-full bg-transparent text-3xl outline-none font-semibold tracking-tight
+            placeholder:text-stone-500'
+          />
+      </form>
+    </div>
   )
 }
 
