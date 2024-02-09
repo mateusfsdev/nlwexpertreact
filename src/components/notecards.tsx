1ae0e49cc17a1c78e449c5342949cf1fe89c8463
mateus sant'ana <mateusfdev@outlook.com>
2024-02-09 18:14:37 -0300

    instal libs date fsn and redix-dialog  🎪🎭🎢
    format date in date fns and create style in dialog portal content

--- src/components/notecards.tsx
+++ src/components/notecards.tsx
@@ -1,3 +1,7 @@
+import * as Dialog from "@radix-ui/react-dialog"
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 interface NoteCardProps {
   //props create object
   note: {
@@ -8,17 +12,34 @@ interface NoteCardProps {
 
 export function NoteCard({note}: NoteCardProps) {
   return(
-    <button className='text-left bg-stone-800 outline-none rounded-md p-5 space-y-3 overflow-hidden relative
-      hover:ring-2 hover:ring-stone-600
-      focus:ring-2 focus:ring-yellow-500'>
-      <span className='text-sm font-medium text-stone-300'>
-        {note.date.toISOString()}
-       
-      </span>
-      <p className='text-sm leading-6 text-stone-400'>
-        {note.content}
-      </p>
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t pointer-events-none from-black/60 to-black/0'></div>
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className='text-left bg-stone-800 outline-none rounded-md p-5 space-y-3 overflow-hidden relative
+        hover:ring-2 hover:ring-stone-600
+        focus:ring-2 focus:ring-yellow-500'>
+        <span className='text-sm font-medium text-stone-300'>
+          {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}  
+        </span>
+        <p className='text-sm leading-6 text-stone-400'>
+          {note.content}
+        </p>
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t pointer-events-none from-black/60 to-black/0'></div>
+      </Dialog.Trigger>
+
+
+      <Dialog.DialogPortal>
+          <Dialog.DialogOverlay className="inset-0 fixed bg-stone-950/60">
+            <Dialog.Content className="fixed left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 h-60vh max-w-[640px] w-full bg-stone-700 rounded-md flex flex-col aoutline-none">
+              <div className="flex flex-1 flex-col gap-3 p-5 ">
+                <span className='text-sm font-medium text-stone-300'>
+                  {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+                </span>
+                <p className='text-sm leading-6 text-stone-400'>
+                  {note.content}
+                </p>
+              </div>
+            </Dialog.Content>
+        </Dialog.DialogOverlay>
+      </Dialog.DialogPortal>
+    </Dialog.Root>
   )
 }
