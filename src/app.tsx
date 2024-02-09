56615b3c0bdc8373fdc40a6840ca2347f60f564f
mateus sant'ana <mateusfdev@outlook.com>
2024-02-09 15:29:46 -0300

    create grid notes 🥁🎷🎷

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import logo from './assets/logo.svg'
 //components react são funções que retorna html em formato JSX
 export function App() {
   return (
-    <div className='mx-auto max-w-6xl my-12'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="Logo site'bloco de notas.voip'" />
 
       <form className='w-full'>
@@ -14,7 +14,19 @@ export function App() {
             placeholder:text-stone-500'
           />
       </form>
-    </div>
+
+      <div className='h-px bg-stone-700' />
+
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+        <div className='bg-stone-700 rounded-md'></div>
+        <div className='bg-stone-800 rounded-md'></div>
+        <div className='bg-stone-800 rounded-md'></div>
+
+
+      </div>
+
+      </div>
+
   )
 }
 
