2b67c874e3c6fef72b53708d0c7f9c323b7fb933
mateus sant'ana <mateusfdev@outlook.com>
2024-02-09 14:35:10 -0300

    tailwind theme config colors ✏✂🖋✒

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+
+
+//components react são funções que retorna html em formato JSX
+export function App() {
+  return (
+    <div className=" bg-gray-950 p-6">
+      <h1 className=" justify-center text-3xl font-bold flex text-bermuda">
+        Voip.notes
+      </h1>
+    </div>
+  )
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'purplehaze': '#8257e6',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
