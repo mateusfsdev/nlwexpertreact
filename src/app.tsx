baafea71055c3624b44c002ed0988b6d37d6b088
mateus sant'ana <mateusfdev@outlook.com>
2024-02-09 23:23:42 -0300

    useState from notes 🏆🏆

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
-import logo from './assets/logo.svg'
-import { NewNote } from './components/newnotecard'
-import { NoteCard } from './components/notecards'
+import { useState } from 'react'
 
-//components react são funções que retorna html em formato JSX
+import { NoteCard } from './components/notecards'
+import { NewNote } from './components/newnotecard'
+
+import logo from './assets/logo.svg'
+
 export function App() {
+  const [notes, setNotes] = useState([
+    {id: 1, date: new Date(), content: 'hello world'},
+    {id: 2, date: new Date(), content: 'segunda nota'}
+  ])
+
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="Logo site'bloco de notas.voip'" />
@@ -22,23 +29,11 @@ export function App() {
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
         <NewNote />
 
-        <NoteCard note={{
-          date: new Date(),
-          content: 'hello world'
-        }}/>
-        <NoteCard note={{
-          date: new Date(),
-          content: 'segunda nota '
-        }}/>
-        <NoteCard note={{
-          date: new Date(),
-          content: 'semana passada nota '
-        }}/>
-
-
+        {notes.map(note =>{
+          return<NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
 
   )
 }
-
